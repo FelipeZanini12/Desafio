86d652b0e86a2b4b8ac2634b64f191338348dbbe
Felipe Festa <felipe.festa31@gmail.com>
2024-10-26 01:46:02 -0300

    feito atualização na lista de tarefas

--- lib/Home.dart
+++ lib/Home.dart
@@ -12,10 +12,11 @@ class TaskListPage extends StatefulWidget {
   _TaskListPageState createState() => _TaskListPageState();
 }
 
-class _TaskListPageState extends State<TaskListPage> {
+class _TaskListPageState extends State<TaskListPage> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
   final _formKey = GlobalKey<FormState>();
-  TextEditingController _titleController = TextEditingController();
-  TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
   DateTime? _dueDate;
   bool _isCompleted = false;
   bool _isEditing = false;
@@ -24,7 +25,16 @@ class _TaskListPageState extends State<TaskListPage> {
   @override
   void initState() {
     super.initState();
+    _tabController = TabController(length: 2, vsync: this);  // Inicializando o TabController
     _loadAllTasks(); // Carrega todas as tarefas ao inicializar a tela
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();  // Liberando o recurso do TabController ao encerrar a página
+    _titleController.dispose();
+    _descriptionController.dispose();
+    super.dispose();
   }
 
   Future<void> _loadAllTasks() async {
@@ -34,6 +44,7 @@ class _TaskListPageState extends State<TaskListPage> {
   void _saveTask() {
     if (_formKey.currentState!.validate()) {
       if (_isEditing && _editingTask != null) {
+        // Atualizando a tarefa existente
         _editingTask!.title = _titleController.text;
         _editingTask!.description = _descriptionController.text;
         _editingTask!.dueDate = _dueDate ?? DateTime.now();
@@ -42,17 +53,18 @@ class _TaskListPageState extends State<TaskListPage> {
         _isEditing = false;
         _editingTask = null;
       } else {
+        // Adicionando uma nova tarefa
         Task newTask = Task(
           id: '', // O ID será gerado no Firestore
           title: _titleController.text,
           description: _descriptionController.text,
           dueDate: _dueDate ?? DateTime.now(),
           isCompleted: _isCompleted,
-          userId: '', // Você pode deixar em branco se não estiver usando userId
+          userId: '', // Pode ser preenchido se necessário
         );
         widget.taskStore.addTask(newTask);
       }
-      _clearForm();
+      _clearForm(); // Limpa o formulário após salvar
     }
   }
 
@@ -62,7 +74,7 @@ class _TaskListPageState extends State<TaskListPage> {
     _isCompleted = false;
     _isEditing = false;
     _editingTask = null;
-    setState(() {});
+    setState(() {}); // Atualiza o estado da página
   }
 
   void _editTask(Task task) {
@@ -75,6 +87,32 @@ class _TaskListPageState extends State<TaskListPage> {
     });
   }
 
+  Future<bool?> _showDeleteConfirmationDialog(BuildContext context) async {
+    return await showDialog<bool>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Confirmar exclusão'),
+          content: const Text('Você tem certeza que deseja excluir esta tarefa?'),
+          actions: [
+            TextButton(
+              child: const Text('Cancelar'),
+              onPressed: () {
+                Navigator.of(context).pop(false); // Cancela a exclusão
+              },
+            ),
+            TextButton(
+              child: const Text('Excluir'),
+              onPressed: () {
+                Navigator.of(context).pop(true); // Confirma a exclusão
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -82,12 +120,12 @@ class _TaskListPageState extends State<TaskListPage> {
         title: Text(_isEditing ? "Editar Tarefa" : "Gerenciamento de Tarefas"),
         backgroundColor: Colors.blue,
       ),
-      body: Container(
-        color: Colors.blue[50],
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Form(
+      body: Column(
+        children: [
+          // Formulário de Adicionar/Editar tarefa (agora em cima das Tabs)
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
               key: _formKey,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -102,8 +140,7 @@ class _TaskListPageState extends State<TaskListPage> {
                         borderRadius: BorderRadius.circular(30),
                         borderSide: BorderSide.none,
                       ),
-                      contentPadding: EdgeInsets.symmetric(
-                          vertical: 16, horizontal: 24),
+                      contentPadding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
                     ),
                     validator: (value) {
                       if (value == null || value.isEmpty) {
@@ -112,7 +149,7 @@ class _TaskListPageState extends State<TaskListPage> {
                       return null;
                     },
                   ),
-                  SizedBox(height: 10),
+                  const SizedBox(height: 10),
                   TextFormField(
                     controller: _descriptionController,
                     decoration: InputDecoration(
@@ -123,106 +160,146 @@ class _TaskListPageState extends State<TaskListPage> {
                         borderRadius: BorderRadius.circular(30),
                         borderSide: BorderSide.none,
                       ),
-                      contentPadding: EdgeInsets.symmetric(
-                          vertical: 16, horizontal: 24),
+                      contentPadding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
                     ),
                   ),
-                  SizedBox(height: 16),
+                  const SizedBox(height: 16),
                   ElevatedButton(
-                    onPressed: _saveTask,
+                    onPressed: _saveTask, // Chama a função de salvar tarefa
                     style: ElevatedButton.styleFrom(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: 32, vertical: 16),
+                      padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(30),
                       ),
                     ),
                     child: Text(
                       _isEditing ? 'Atualizar Tarefa' : 'Adicionar Tarefa',
-                      style: TextStyle(fontSize: 18),
+                      style: const TextStyle(fontSize: 18),
                     ),
                   ),
                 ],
               ),
             ),
-            SizedBox(height: 20),
-            Expanded(
-              child: Observer(
-                builder: (_) {
-                  if (widget.taskStore.taskList.isEmpty) {
-                    return Center(child: Text("Nenhuma tarefa encontrada."));
-                  } else {
-                    return ListView.builder(
-                      itemCount: widget.taskStore.taskList.length,
-                      itemBuilder: (context, index) {
-                        final task = widget.taskStore.taskList[index];
-                        return Dismissible(
-                          key: Key(task.id),
-                          background: Container(
-                            color: Colors.redAccent,
-                            alignment: Alignment.centerRight,
-                            padding: EdgeInsets.symmetric(horizontal: 20),
-                            child: Icon(Icons.delete, color: Colors.white),
-                          ),
-                          direction: DismissDirection.endToStart,
-                          onDismissed: (direction) {
-                            widget.taskStore.deleteTask(task.id);
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(content: Text('Tarefa excluída')),
-                            );
-                          },
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 8.0),
-                            child: Card(
-                              elevation: 4,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20),
-                              ),
-                              child: ListTile(
-                                contentPadding: EdgeInsets.symmetric(
-                                    vertical: 16, horizontal: 16),
-                                title: Text(
-                                  task.title,
-                                  style: TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w600),
-                                ),
-                                subtitle: Text(task.description),
-                                trailing: Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    IconButton(
-                                      icon: Icon(Icons.edit, color: Colors.blue),
-                                      onPressed: () {
-                                        _editTask(task); // Função para editar a tarefa
-                                      },
-                                    ),
-                                    Checkbox(
-                                      value: task.isCompleted,
-                                      onChanged: (bool? value) {
-                                        task.isCompleted = value ?? false;
-                                        widget.taskStore.updateTask(task); // Atualiza a tarefa
-                                      },
-                                      activeColor: Colors.blue,
-                                    ),
-                                  ],
-                                ),
-                                onTap: () {
-                                  _editTask(task); // Função para editar a tarefa
-                                },
-                              ),
-                            ),
-                          ),
-                        );
+          ),
+          // Tabs para separar as tarefas pendentes e concluídas
+          TabBar(
+            controller: _tabController,
+            tabs: const [
+              Tab(text: 'Pendentes'),
+              Tab(text: 'Concluídas'),
+            ],
+          ),
+          Expanded(
+            child: Observer(
+              builder: (_) {
+                var pendingTasks = widget.taskStore.taskList
+                    .where((task) => !task.isCompleted) // Filtra as tarefas pendentes
+                    .toList();
+                var completedTasks = widget.taskStore.taskList
+                    .where((task) => task.isCompleted) // Filtra as tarefas concluídas
+                    .toList();
+
+                return TabBarView(
+                  controller: _tabController,
+                  children: [
+                    _buildTaskList(pendingTasks),  // Lista de Tarefas Pendentes
+                    _buildTaskList(completedTasks), // Lista de Tarefas Concluídas
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTaskList(List<Task> tasks) {
+    if (tasks.isEmpty) {
+      return const Center(
+        child: Text("Nenhuma tarefa encontrada."),
+      );
+    }
+
+    return ListView.builder(
+      itemCount: tasks.length,
+      itemBuilder: (context, index) {
+        final task = tasks[index];
+        return Dismissible(
+          key: Key(task.id),
+          background: Container(
+            color: Colors.redAccent,
+            alignment: Alignment.centerRight,
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: const Icon(Icons.delete, color: Colors.white),
+          ),
+          direction: DismissDirection.endToStart,
+          confirmDismiss: (direction) async {
+            // Exibe o diálogo de confirmação
+            final confirm = await _showDeleteConfirmationDialog(context);
+            if (confirm == true) {
+              widget.taskStore.deleteTask(task.id);
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('Tarefa excluída')),
+              );
+              return true; // Confirma a exclusão
+            }
+            return false; // Cancela a exclusão
+          },
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: Card(
+              elevation: 4,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: ListTile(
+                contentPadding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+                title: Text(
+                  task.title,
+                  style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                ),
+                subtitle: Text(task.description),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.edit, color: Colors.blue),
+                      onPressed: () {
+                        _editTask(task); // Função para editar a tarefa
                       },
-                    );
-                  }
+                    ),
+                    Checkbox(
+                      value: task.isCompleted,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          // Atualiza localmente para refletir imediatamente na UI
+                          task.isCompleted = value ?? false;
+                        });
+
+                        // Atualiza o Firestore em segundo plano
+                        widget.taskStore.updateTask(task).catchError((error) {
+                          // Em caso de erro, reverte a mudança no estado local
+                          setState(() {
+                            task.isCompleted = !task.isCompleted;
+                          });
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text('Erro ao atualizar tarefa: $error')),
+                          );
+                        });
+                      },
+                      activeColor: Colors.blue,
+                    ),
+                  ],
+                ),
+                onTap: () {
+                  _editTask(task); // Função para editar a tarefa
                 },
               ),
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
